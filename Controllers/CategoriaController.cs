7d4d718cbd5b9a4b5c5d0afff17275529e68ac31
WILL-29 <willdelarosa29@hotmail.com>
2023-04-27 21:25:09 -0400

    Metodos Categoria
    Metodo Actualizar Categoría
    Metodo Eliminar Categoría

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -56,6 +56,10 @@ namespace Api_Peliculas.Controllers
             return Ok(categoria);
         }
         [HttpPost]
+        [ProducesResponseType(201, Type = typeof(CategoriaDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]        
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult CrearCategoria([FromBody] CrearCategoriaDto categoriaDto)
         {
             if (!ModelState.IsValid)
@@ -71,17 +75,60 @@ namespace Api_Peliculas.Controllers
             if (_ctRepo.ExisteCategoria(categoriaDto.Nombre_Cat))
             {
                 ModelState.AddModelError("","La categoría existe");
-                return StatusCode(404, ModelState);
+                return StatusCode(400, ModelState);
             } 
             Categoria categoria = _mapper.Map<Categoria>(categoriaDto);
             if (!_ctRepo.CrearCategoria(categoria))
             {
-                ModelState.AddModelError("","Algo salió mal creando la categoría");
+                ModelState.AddModelError("","Algo salió mal creando la categoría " + categoriaDto.Nombre_Cat);
                 return StatusCode(500, ModelState);                
             }
             //return Ok();  
             return CreatedAtAction("GetCategoria", new { id = categoria.ID_Categoria }, categoria);            
         }
-        //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        
+        [HttpPatch("{id:int}", Name="ActualizarCategoriaPath")]
+        [ProducesResponseType(201, Type = typeof(CategoriaDto))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult ActualizarCategoriaPath(int id, [FromBody] CategoriaDto categoriaDto)
+        {
+             if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (categoriaDto == null  || id != categoriaDto.ID_Categoria)
+            {
+                return BadRequest(ModelState);
+            }
+            Categoria categoria = _mapper.Map<Categoria>(categoriaDto);
+            if (!_ctRepo.ActualizarCategoria(categoria))
+            {
+                ModelState.AddModelError("","Algo salió mal actualizando la categoría " + categoria.Nombre_Cat);
+                return StatusCode(500, ModelState);                
+            }        
+            return NoContent();            
+        }
+        [HttpDelete("{id:int}", Name = "BorrarCategoria")]
+        public IActionResult BorrarCategoria(int id)
+        {   
+            if (!_ctRepo.ExisteCategoria(id))
+            {
+                return NotFound();
+            }        
+            var categoria = _ctRepo.GetCategoria(id);            
+            if (id != categoria.ID_Categoria)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!_ctRepo.BorrarCategoria(categoria))
+            {
+                ModelState.AddModelError("","Algo salió mal eliminando la categoría");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+        
     }
 }
